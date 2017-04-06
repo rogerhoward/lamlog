8a28bd1127a11c5addb6bf4698448094ac51a158
Roger Howard <rogerhoward@mac.com>
2017-04-06 10:03:11 -0700

    working logger

--- server.py
+++ server.py
@@ -11,8 +11,7 @@ import time
 from pprint import pprint
 
 app = flask.Flask(__name__)
-
-    s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 
 #------------------------------------------------#
 #  Bot endpoint                                  #
@@ -26,18 +25,25 @@ def log():
 
     # Grab form data from Slack inbound, and pass it to plugin dispatch
     # Payload format documented at https://api.slack.com/slash-commands#how_do_commands_work
+    r = flask.request
 
-    request_data = flask.request.values.to_dict(flat=False)
-    print(request_data)
+    data = {}
+
+    data['form'] = r.form.to_dict(flat=False)
+    data['args'] = r.args.to_dict(flat=False)
+    data['cookies'] = r.cookies
+    data['headers'] = dict(r.headers)
+    data['files'] = r.files.to_dict(flat=False)
+    data['json'] = r.get_json()
+
+    print(data)
 
     io = StringIO.StringIO()
+    json.dump(data, io, sort_keys=True, indent=4)
 
-    json.dump(request_data, io)
+    key = '{}.json'.format(int(round(time.time() * 1000)))
 
-    time_ms = int(round(time.time() * 1000))
-
-    key = '{}.json'.format(time_ms)
-
+    io.seek(0)
     s3.upload_fileobj(io, config.S3_LOG_BUCKET, key)
 
     response = flask.Response()
